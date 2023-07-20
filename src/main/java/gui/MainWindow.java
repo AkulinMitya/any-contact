24bf13434bcb8094843c3ffb511518a37cf0b890
Akulin_Dmitry <newmitya2@gmail.com>
2023-07-20 11:55:29 +0500

    raw timer

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -2,18 +2,33 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame{
+    private JLabel timerLabel;
+    private Timer timer;
+    private int secondsRemaining;
     public MainWindow() {
         super("Any contact");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300,300);
         setVisible(true);
 
-        getContentPane().add(BorderLayout.NORTH, createWord(
-                "Yura", 450, 250, "Verdana", 25
-        ));
+        secondsRemaining = 0;
+        timerLabel = createTimerLabel(450, 250, "Verdana", 25);
+        getContentPane().add(BorderLayout.NORTH, createWord("Yura", 450, 250, "Verdana", 25));
+        getContentPane().add(BorderLayout.CENTER, timerLabel);
         getContentPane().add(BorderLayout.SOUTH, createButtonsPanel());
+    }
+
+    private JLabel createTimerLabel(
+            int width, int height, String font, int fontSize
+    ) {
+        JLabel timerLabel = new JLabel("", SwingConstants.CENTER);
+        timerLabel.setPreferredSize(new Dimension(width, height));
+        timerLabel.setFont(new Font(font, Font.PLAIN, fontSize));
+        return timerLabel;
     }
 
     private JButton generateButton(
@@ -31,13 +46,35 @@ public class MainWindow extends JFrame{
         buttonsPanel.add(generateButton(
                 "You guess the letter!", 450, 250, "Verdana", 25)
         );
-        buttonsPanel.add(generateButton(
-                "Any Contact?!", 450, 250, "Verdana", 25)
+        JButton anyContactButton = generateButton(
+                "Any Contact?!", 450, 250, "Verdana", 25
         );
+        addAnyContactListener(anyContactButton);
+        buttonsPanel.add(anyContactButton);
 
         return buttonsPanel;
     }
 
+    private void addAnyContactListener(JButton anyContactButton){
+        anyContactButton.addActionListener(e -> {
+            // Запускаем таймер на 5 секунд
+            secondsRemaining = 5;
+            if (timer != null) {
+                timer.stop();
+            }
+
+            timer = new Timer(1000, e1 -> {
+                if (secondsRemaining > 0) {
+                    timerLabel.setText("Осталось времени: " + secondsRemaining + " сек");
+                    secondsRemaining--;
+                } else {
+                    timerLabel.setText("Таймер завершен!");
+                    timer.stop();
+                }
+            });
+            timer.start();
+        });
+    }
     private JLabel createWord(
             String text, int width, int height, String font, int fontSize
     ) {
